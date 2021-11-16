ae9106071b755a3a2d25d6bdff3d086a65105599
akashita-s <82257553+akashita-s@users.noreply.github.com>
2021-11-17 01:30:09 +0530

    Setup done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBJK5AWXV55wVryXu37_SRj8vQBJ4CCMhs",
+    authDomain: "candy-messenger.firebaseapp.com",
+    projectId: "candy-messenger",
+    storageBucket: "candy-messenger.appspot.com",
+    messagingSenderId: "205088726049",
+    appId: "1:205088726049:web:159802f1e1ad0965d85760",
+    measurementId: "G-Z3X9VQ6ZEB"
+  };
